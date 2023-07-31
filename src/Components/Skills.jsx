eca14cf61a968b4f2f6f7e77187a5a68438feab5
DerianBustos17 <derianbustos2002@gmail.com>
2023-07-31 13:15:10 -0600

    Add the Aboutme with carrousel of skills

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+
+const Skills = () => {
+   
+
+  return (
+    
+    <div className="central ">
+      <div className="conteiner">
+        <div className="carta">
+            <img className='w-full' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png" alt="" />
+        </div>
+        <div className="carta">
+            <img className='w-full'  src="https://ayso32m5n.cloudimg.io/v7/https://web-id.fr/storage/logos%2Flaravel-mark-red-type-black_w1280.png?width=900" alt="" />
+        </div>
+        <div className="carta">
+           <img className='w-full' src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png"></img>
+        </div>
+        <div className="carta">
+            <img className='w-full ' src="https://techinfini.in/wp-content/uploads/2017/09/React-Logo-1.png" alt="" />
+        </div>
+        <div className="carta">
+            <img className='w-full' src="https://1000marcas.net/wp-content/uploads/2020/11/Java-logo.png" alt="" />
+        </div>
+        <div className="carta">
+            <img className='w-full' src="https://programandoointentandolo.com/wordpress/wp-content/uploads/2017/10/css3-logo.svg" alt="" /></div>
+        <div className="carta">
+            <img className='w-full' src="https://www.freepnglogos.com/uploads/javascript-png/png-javascript-badge-picture-8.png" alt="" />
+        </div>
+      </div>
+    </div>
+ 
+  );
+};
+
+export default Skills;
