d04421cb211f834ff33c7d20ba902b44759519e7
DerianBustos17 <derianbustos2002@gmail.com>
2023-08-07 17:30:13 -0600

    Se actualiza el pdf

--- src/Components/ButtomDownloadCV.jsx
+++ src/Components/ButtomDownloadCV.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ButtomDownloadCV = () => { 
     return (
-        <a href='/CV_DerianBustos.pdf' target="_blank" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded flex items-center w-full md:w-56 md:justify-center">
+        <a href='/CV_DerianBustosR.pdf' target="_blank" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded flex items-center w-full md:w-56 md:justify-center">
           <svg className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
           </svg>
